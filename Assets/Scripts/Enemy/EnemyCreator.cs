79714c5f661ba62ead0774cd3b2da35460f6e41d
Jürnas Pluutus <JyrnasPluutus@gmail.com>
2021-04-05 21:20:32 +0300

    Refactor spawn timing logic out of EnemyCreator into EnemySpawner

--- Assets/Scripts/Enemy/EnemyCreator.cs
+++ Assets/Scripts/Enemy/EnemyCreator.cs
@@ -11,60 +11,19 @@ namespace Enemy
         public float enemySpeed = 15f;
         private float _enemySpeedAdd = 5f;
         
-        private float _enemySpawnTimeIncrement = 0.25f;
-        
-        public float pauseBetweenEnemySpawns = 2.5f;
-        private float _enemySpawnTimeoutEnd;
-        
-        public float pauseBetweenSpawnCycles = 5f;
-        private float _spawnCyclePauseEnd;
         
         private Random _random;
-
-        private int _spawnCount;
-        private const int SpawnCycleSize = 10;
-
         public void Start()
         {
-            _enemySpawnTimeoutEnd = Time.time;
             _random = new Random();
         }
 
-        public void Update()
-        {
-            if (IsSpawnCyclePause()) {return;}
-
-            if (CanSpawnEnemy())
-            {
-                SpawnRandomEnemy();
-            }
-        }
-
-        private bool IsSpawnCyclePause()
-        {
-            return Time.time < _spawnCyclePauseEnd;
-        }
-
-        private bool CanSpawnEnemy()
-        {
-            return Time.time > _enemySpawnTimeoutEnd;
-        }
-
-        private void SpawnRandomEnemy()
+        public void SpawnRandomEnemy()
         {
             var enemy = Instantiate(GetRandomEnemyPrefab());
+            
             enemy.transform.position += transform.position;
             enemy.GetComponent<EnemyMove>().speed = enemySpeed;
-            
-            _spawnCount++;
-            
-            if (HasSpawnedEnoughEnemiesForSpawnCyclePause())
-            {
-                StartSpawnCyclePause();
-                IncreaseDifficulty();
-                return;
-            }
-            StartPauseBetweenSpawns();
         }
 
         private GameObject GetRandomEnemyPrefab()
@@ -74,35 +33,9 @@ namespace Enemy
             return enemy;
         }
 
-        private bool HasSpawnedEnoughEnemiesForSpawnCyclePause()
-        {
-            return _spawnCount % SpawnCycleSize == 0;
-        }
-
-        private void StartSpawnCyclePause()
-        {
-            _spawnCyclePauseEnd = Time.time + pauseBetweenSpawnCycles;
-        }
-
-        private void IncreaseDifficulty()
-        {
-            IncreaseSpeed();
-            DecreaseTimeBetweenSpawns();
-        }
-
         public void IncreaseSpeed()
         {
             enemySpeed += _enemySpeedAdd;
-        }
-
-        public void DecreaseTimeBetweenSpawns()
-        {
-            pauseBetweenEnemySpawns -= _enemySpawnTimeIncrement;
-        }
-
-        private void StartPauseBetweenSpawns()
-        {
-            _enemySpawnTimeoutEnd = Time.time + pauseBetweenEnemySpawns;
         }
     }
 }
---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        public EnemyCreator enemyCreator;
+        
+        public float pauseBetweenEnemySpawns = 2.5f;
+        private float _enemySpawnTimeoutEnd;
+        
+        public float pauseBetweenSpawnCycles = 5f;
+        private float _spawnCyclePauseEnd;
+        private const float EnemySpawnTimeIncrement = 0.25f;
+
+        private int _spawnCount;
+        private const int SpawnCycleSize = 10;
+        
+        public void Update()
+        {
+            if (IsSpawnCyclePause()) return;
+
+            if (IsEnemySpawnTimeout()) return;
+            
+            enemyCreator.SpawnRandomEnemy();
+            
+            _spawnCount++;
+            
+            if (HasSpawnedEnoughEnemiesForSpawnCyclePause())
+            {
+                StartSpawnCyclePause();
+                IncreaseDifficulty();
+                return;
+            }
+            StartPauseBetweenSpawns();
+        }
+
+        private bool IsSpawnCyclePause()
+        {
+            return Time.time < _spawnCyclePauseEnd;
+        }
+
+        private bool IsEnemySpawnTimeout()
+        {
+            return Time.time < _enemySpawnTimeoutEnd;
+        }
+
+        private bool HasSpawnedEnoughEnemiesForSpawnCyclePause()
+        {
+            return _spawnCount % SpawnCycleSize == 0;
+        }
+
+        private void StartSpawnCyclePause()
+        {
+            _spawnCyclePauseEnd = Time.time + pauseBetweenSpawnCycles;
+        }
+
+        private void IncreaseDifficulty()
+        {
+            enemyCreator.IncreaseSpeed();
+            DecreaseTimeBetweenSpawns();
+        }
+
+        private void DecreaseTimeBetweenSpawns()
+        {
+            pauseBetweenEnemySpawns -= EnemySpawnTimeIncrement;
+        }
+
+        private void StartPauseBetweenSpawns()
+        {
+            _enemySpawnTimeoutEnd = Time.time + pauseBetweenEnemySpawns;
+        }
+    }
+}
